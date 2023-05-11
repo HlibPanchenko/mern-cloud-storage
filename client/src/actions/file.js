28aa5ae2924d550c3c4f44ad98f059edb9cf5f6d
HlibPanchenko <glebpanchenk7@gmail.com>
2023-05-11 18:21:30 +0300

    add client file upload

--- client/src/actions/file.js
+++ client/src/actions/file.js
@@ -1,36 +1,91 @@
 // функцию получения файлов с сервера
-import axios from 'axios'
-import {setFiles,addFile} from "../reducers/fileReducer";
+import axios from "axios";
+import { setFiles, addFile } from "../reducers/fileReducer";
 
 // передаем id текущей директории
 export function getFiles(dirId) {
-// вернем ассинхронную функцию, которая принимает dispatch
-    return async dispatch => {
-        try {
-            const response = await axios.get(`http://localhost:5000/api/files${dirId ? '?parent='+dirId : ''}`, {
-                headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}
-					 // по токену будем идентифицировать пользователя на сервере
-            })
-            dispatch(setFiles(response.data))
-        } catch (e) {
-            alert(e.response.data.message)
+  // вернем ассинхронную функцию, которая принимает dispatch
+  return async (dispatch) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:5000/api/files${dirId ? "?parent=" + dirId : ""}`,
+        {
+          headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+          // по токену будем идентифицировать пользователя на сервере
         }
+      );
+      dispatch(setFiles(response.data));
+    } catch (e) {
+      alert(e.response.data.message);
     }
+  };
 }
 
 export function createDir(dirId, name) {
-	return async dispatch => {
-		 try {
-			  const response = await axios.post(`http://localhost:5000/api/files`,{
-					name,
-					parent: dirId, // id родительской папки
-					type: 'dir'
-			  }, {
-					headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}
-			  })
-			  dispatch(addFile(response.data))
-		 } catch (e) {
-			  alert(e.response.data.message)
-		 }
-	}
+  return async (dispatch) => {
+    try {
+      const response = await axios.post(
+        `http://localhost:5000/api/files`,
+        {
+          name,
+          parent: dirId, // id родительской папки
+          type: "dir",
+        },
+        {
+          headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+        }
+      );
+      dispatch(addFile(response.data));
+    } catch (e) {
+      alert(e.response.data.message);
+    }
+  };
+}
+
+export function uploadFile(file, dirId) {
+  return async (dispatch) => {
+    try {
+      // тело запроса
+      const formData = new FormData();
+      formData.append("file", file);
+      // если id текущей директории существует, тогда в FormData мы помещаем еще и его
+      if (dirId) {
+        formData.append("parent", dirId);
+      }
+      // объект formData передаем вторым параметром в post запрос
+      const response = await axios.post(
+        `http://localhost:5000/api/files/upload`,
+        formData,
+        {
+          headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+          // проценты загрузки файла
+          onUploadProgress: (progressEvent) => {
+            const totalLength = progressEvent.lengthComputable
+              ? progressEvent.total
+              : progressEvent.target?.getResponseHeader("content-length") ||
+                progressEvent.target?.getResponseHeader(
+                  "x-decompressed-content-length"
+                );
+            // const totalLength = progressEvent.lengthComputable
+            //   ? progressEvent.total
+            //   : progressEvent.target.getResponseHeader("content-length") ||
+            //     progressEvent.target.getResponseHeader(
+            //       "x-decompressed-content-length"
+            //     );
+            console.log("total", totalLength);
+            if (totalLength) {
+              let progress = Math.round(
+                (progressEvent.loaded * 100) / totalLength
+              );
+              console.log(progress);
+            }
+          },
+        }
+      );
+      // диспатчим в редакс
+      dispatch(addFile(response.data));
+    } catch (e) {
+      alert(e.response.data.message);
+    }
+  };
 }
--- client/src/components/disk/Disk.jsx
+++ client/src/components/disk/Disk.jsx
@@ -1,43 +1,67 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getFiles, createDir } from "../../actions/file";
+import { getFiles, createDir, uploadFile } from "../../actions/file";
 import FileList from "./fileList/FileList";
 import "./disk.scss";
 import Popup from "./Popup";
-import {setPopupDisplay, setCurrentDir} from "../../reducers/fileReducer";
+import { setPopupDisplay, setCurrentDir } from "../../reducers/fileReducer";
 
 const Disk = () => {
   const dispatch = useDispatch();
   const currentDir = useSelector((state) => state.files.currentDir);
-  // Каждый раз, когда мы открываем папку, мы будем закидывать ее id 
+  // Каждый раз, когда мы открываем папку, мы будем закидывать ее id
   // в массив dirStack. Таким образом у нас будет путь папок.
-  const dirStack = useSelector(state => state.files.dirStack)
+  const dirStack = useSelector((state) => state.files.dirStack);
 
   useEffect(() => {
     dispatch(getFiles(currentDir));
   }, [currentDir]);
 
-  // функция открытия модального окна 
+  // функция открытия модального окна
   function showPopupHandler() {
-	dispatch(setPopupDisplay('flex'))
-}
+    dispatch(setPopupDisplay("flex"));
+  }
 
-// возвращение назад (в прошлую папку)
-function backClickHandler() {
-	// получаем ID последней открытой папки 
-	const backDirId = dirStack.pop()
-	// делаем эту папку текущей 
-	dispatch(setCurrentDir(backDirId))
+  // возвращение назад (в прошлую папку)
+  function backClickHandler() {
+    // получаем ID последней открытой папки
+    const backDirId = dirStack.pop();
+    // делаем эту папку текущей
+    dispatch(setCurrentDir(backDirId));
+  }
+
+  // вызывается при выборе файлов в input
+  function fileUploadHandler(event) {
+  // в массив прокинем список файлов из input
+	const files = [...event.target.files]
+  // для каждого из этих файлов вызовем функцию загрузки 
+	files.forEach(file => dispatch(uploadFile(file, currentDir)))
 }
 
   return (
     <div className="disk">
       <div className="disk__btns">
-        <button className="disk__back" onClick={() => backClickHandler()}>Назад</button>
-        <button className="disk__create" onClick={() => showPopupHandler()}>Создать папку</button>
+        <button className="disk__back" onClick={() => backClickHandler()}>
+          Назад
+        </button>
+        <button className="disk__create" onClick={() => showPopupHandler()}>
+          Создать папку
+        </button>
+        <div className="disk__upload">
+          <label htmlFor="disk__upload-input" className="disk__upload-label">
+            Загрузить файл
+          </label>
+          <input
+            multiple={true} // пользователь может выбрать сразу несколько файлов
+            onChange={(event) => fileUploadHandler(event)}
+            type="file"
+            id="disk__upload-input"
+            className="disk__upload-input"
+          />
+        </div>
       </div>
       <FileList />
-		<Popup/>
+      <Popup />
     </div>
   );
 };
